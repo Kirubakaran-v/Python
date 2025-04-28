369e86018f9dfcfb09b89b2b7fd0c8390e509911
Kirubakaran Vethamoorthi <kirubakaran.choco@gmail.com>
2025-04-28 13:25:14 +0530

    learning

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
-x = "Python"
-y= "is"
-z = "awesome"
-print (x,y,z)
-print (x + y + z)
+# x = "Python"
+# y= "is"
+# z = "awesome"
+# print (x,y,z)
+# print (x + y + z)
+
+# x = str(5)
+# y = "kiruba"
+# print (x + y)
+
+# x = False
+# print(type(x))
+
+# y = 2.8
+# print(int(y))
+
+x = 5
+print(float(x))
