8fec6d927f6fe58eb1dddc890722a63683befd0d
Kirubakaran Vethamoorthi <kirubakaran.choco@gmail.com>
2025-04-28 14:50:36 +0530

    added some string learning

---
+++ string.py
@@ -0,0 +1,11 @@
+# a = "Vinod void"
+# print(len(a))
+
+# print(str(len(a) - 1))
+
+# # find a lenth of the string 
+# b = len(a)
+# # find a index of the last charac d 
+# c = b - 1
+# # print the character
+# print(a[c])
