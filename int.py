5c2e9155ea846646a9d586cc7ef99951d4f92dc3
Kirubakaran Vethamoorthi <kirubakaran.choco@gmail.com>
2025-04-29 17:58:18 +0530

    learning

--- int.py
+++ int.py
@@ -16,3 +16,30 @@
 
 # x = 5
 # print(float(x))
+
+# def swap_case(s):
+#         print(dpfgpofgik
+#     return s.swapcase()
+
+# if __name__ == '__main__':
+#     s = input()
+#     result = swap_case(s)
+#     print(result)
+
+# def split_and_join(line):
+#     # write your code here
+#     split = line.split(" ")
+#     return "-".join(split) 
+# if __name__ == '__main__':
+#     line = input()
+#     result = split_and_join(line)
+#     print(result)
+
+def print_full_name(first, last):
+    # Write your code here
+    
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
