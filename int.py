05fc4cc63aff1cfed0bac65d4c2e0847d18f4f9e
Kirubakaran Vethamoorthi <kirubakaran.choco@gmail.com>
2025-04-30 00:37:47 +0530

    learning

--- int.py
+++ int.py
@@ -35,11 +35,14 @@
 #     result = split_and_join(line)
 #     print(result)
 
-def print_full_name(first, last):
-    # Write your code here
+# def print_full_name(first, last):
+#     # Write your code here
+#     return f"Hello {first} {last}! You just delved into python."
     
+# if __name__ == '__main__':
+#     first_name = input()
+#     last_name = input()
+#     print_full_name(first_name, last_name)
+#     output = print_full_name(first_name, last_name)
+#     print(output)
 
-if __name__ == '__main__':
-    first_name = input()
-    last_name = input()
-    print_full_name(first_name, last_name)
