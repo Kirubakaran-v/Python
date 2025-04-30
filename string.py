380b0e9a28662c63592ec74a38cfe231b5c8f7b6
Kirubakaran Vethamoorthi <kirubakaran.choco@gmail.com>
2025-04-30 16:19:12 +0530

    learning

--- string.py
+++ string.py
@@ -19,6 +19,28 @@
 # txt = f"The price is {price} dollars"
 # print(txt)
 
-price = 59
-txt = "The price is " + str(price) + " dollars"
-print(txt)
+# price = 59
+# txt = "The price is " + str(price) + " dollars"
+# print(txt)
+
+# s = "ab123"
+# print(s.isalnum())
+# import textwrap
+# string = 'ABCDEFGHIJKLIMNOQRSTUVWXYZ'
+# max_width = 4
+# wrap_text=textwrap.wrap(string, width=max_width)
+# for i in wrap_text:
+#     print(i)
+
+# import textwrap
+
+# def wrap(string, max_width):
+#     wrap_text=textwrap.wrap(string, width=max_width)
+#     for i in wrap_text:
+#         output = i
+#         return 
+
+# if __name__ == '__main__':
+#     string, max_width = input(), int(input())
+#     result = wrap(string, max_width)
+#     print(result)
