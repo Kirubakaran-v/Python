52c802b57c93df23af07c7a36c082a3f518f1eab
Kirubakaran Vethamoorthi <kirubakaran.choco@gmail.com>
2025-05-02 11:57:13 +0530

    Functions

---
+++ function.py
@@ -0,0 +1,22 @@
+# def my_function(argument, test):
+#     print("Hello from function!")
+#     print(f"{argument} Hello from function!")
+#     print(test*5)
+
+# my_function("Welcome to Python learning", 5)
+
+# my_variable="Welcome to python Learning"
+# my_variable2="This is day 4"
+# my_variable3="Got into Functions"
+# def test_function(arg1, arg2, arg3):
+#     print(f"{my_variable}, {my_variable2}, {my_variable3}")
+
+# test_function(my_variable, my_variable2, my_variable3)
+
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
