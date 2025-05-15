2a03bca8289c70bfb8ae98d54ef14da2ba53b40b
Kirubakaran Vethamoorthi <kirubakaran.choco@gmail.com>
2025-05-15 18:18:30 +0530

    new file

--- find_function.py
+++ find_function.py
@@ -8,19 +8,18 @@ def find_all(name, path):
     return result
 
 def main():
-
     # Input Variables
     file_name = 'schema.sql'
     file_path = '/Users/Vkirubakaran/Downloads/test_schema_search'
     output_file = '/Users/Vkirubakaran/Downloads/merged_schema.sql'
 
-    matched_results=find_all(file_name, file_path)
-    for f in matched_results:
-        with open(f) as content:
-                print(content.read())
+    matching_files = find_all(file_name, file_path)
+
+    with open(output_file, 'w') as outf:
+        for file in matching_files:
+            with open(file, 'r') as f:
+                outf.write(f.read())
+                outf.write("\n\n")
 
 if __name__ == "__main__":
     main()
-
-            
-
